eec6c8014d3bbf0b2c92052a93e11656ef8d4411
nosdivad21 <jacksondavidson215@gmail.com>
2024-10-24 17:15:17 -0500

    Commit

--- Strings.java
+++ Strings.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+
 public class Strings {
-    public static void main(String[} args]) {
+    public static void main(String[] args) {
+        String[] cars = new String[3];
+        String[] cars1 = {"A", "B", "C"};
+        ArrayList<String> cars3 = new ArrayList<>();
+
+        String name = "Jackson Davidson";
+        System.out.println(name);
+
     }
 }
